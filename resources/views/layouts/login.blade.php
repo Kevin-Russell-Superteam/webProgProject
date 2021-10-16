010e7d2e88b3bc5c6061f36fdd2bdc6a3d3a0aa1
kevinorlando88 <kevinorlando312@gmail.com>
2021-10-16 14:23:10 +0700

    Nambahin Login

---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.main')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <div class="judul">
+        <h1>Login</h1>
+    </div>
+    
+    <form action="#">
+        <label for="email">Email</label>
+        <input type="text" class="inputEmail">
+        <label for="password">Password</label>
+        <input type="text" class="inputPassword">
+        <input type="checkbox" class="type"> Remember Me
+        <input type="button" class="type">Login
+    </form>
+    
+    </div>
+</body>
+</html>
